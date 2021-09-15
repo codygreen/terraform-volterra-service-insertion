62e0f237e68698988c1ed3a45ee2fca41c2c1a66
Cody Green <cody@codygreen.com>
2021-09-15 11:17:41 -0500

    remove old content

--- big-ip-configuration/output.tf
+++
@@ -1,3 +0,0 @@
-output "test" {
-  value = var.mgmtPublicIP.value[0]
-}
--- big-ip-configuration/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "public_addresses" {}
-variable "private_addresses" {}
-variable "bigip_password" {}
-variable "mgmtPublicURL" {}
-variable "f5_username" {}
-variable "mgmtPort" {}
-variable "mgmtPublicDNS" {}
-variable "mgmtPublicIP" {}
